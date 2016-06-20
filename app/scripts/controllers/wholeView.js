6dfd7e00b57ab02dd5d037aacecd5e8ce6b239b7
Yasunori Sannomiya <yasunori@sannomiya03.com>
2016-06-20 14:06:11 +0900

    add toDetail function

--- app/scripts/controllers/wholeView.js
+++ app/scripts/controllers/wholeView.js
@@ -1,13 +1,18 @@
 (function(){
 	var app = angular.module("c.wholeView",[]);
 
-	app.controller("WholeViewController", function( $scope, Items, Item, ModalState ){
-		var self = this;
-		self.Items = Items;
-		self.Item = Item;
-
+	app.controller("WholeViewController", function( $scope, $location, Items, Item, ModalState ){
 		$scope.$on('$viewContentLoaded', function(event){
 			console.log("CULLENT VIEW MODE > Whole view");
 		});
+		var self = this;
+		self.Items = Items;
+		self.Item = Item;
+		self.toDetail = toDetail;
+
+		function toDetail( item ){
+			console.log( "To Detail Page: S"+item.s_no+"/"+item.branch_no );
+			$location.path("detail/S"+item.s_no+"/"+item.branch_no);
+		}
 	});
 })();
